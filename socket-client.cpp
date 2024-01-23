4351b1455a217548d68dec217e7245e3c55f4072
danym <dannymanastireanu@gmail.com>
2024-01-23 22:39:50 +0200

    Working structure

--- socket-client.cpp
+++ socket-client.cpp
@@ -6,9 +6,11 @@
 #include <arpa/inet.h>
 
 #include <infiniband/verbs.h>
+using namespace std;
 
 const char* SERVER_IP = "192.168.1.132";
 const int PORT = 8080;
+const int BUFFER_SIZE = 1024;
 
 struct device_info
 {
@@ -16,10 +18,37 @@ struct device_info
 	uint32_t send_qp_num;
 };
 
+struct ibv_device** get_rxe_device() {
+	int num_devices;
+	struct ibv_device** dev_list = ibv_get_device_list(&num_devices);
+	cout << "Found " << num_devices << " device(s)" << endl;
+	if (!dev_list || num_devices != 1)
+	{
+		cerr << "ibv_get_device_list failed or number of devices != 1" << strerror(errno) << endl;
+		exit(1);
+	}
+	cout << "Interface to use: " << dev_list[0]->name << " more info found at: " << dev_list[0]->ibdev_path << endl;
+	return dev_list;
+}
 
 int main() {
     int clientSocket;
     struct sockaddr_in serverAddr;
+    ssize_t bytesRead;
+    const char* message = "[CLIENT] RDMA device info about client";
+
+
+    // ==== RDMA variables ====
+    struct ibv_device** dev_list = get_rxe_device();
+	struct ibv_context *context = ibv_open_device(dev_list[0]);
+	struct ibv_pd *pd = ibv_alloc_pd(context);
+	
+    if (!pd)
+	{
+		cerr << "ibv_alloc_pd failed: " << strerror(errno) << endl;
+		goto free_context;
+	}
+
 
     // Create socket
     if ((clientSocket = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
@@ -40,15 +69,60 @@ int main() {
     }
 
     // Send a message to the server
-    const char* message = "Hello, server!";
+    cout << "> Send RDMA device info to MASTER" << endl;
     if (send(clientSocket, message, strlen(message), 0) == -1) {
         perror("Message sending failed");
-    } else {
-        std::cout << "Message sent to the server: " << message << std::endl;
     }
 
-    // Close the client socket
-    close(clientSocket);
 
+    // Read from server where can I send some data
+    char buffer[BUFFER_SIZE];
+    bytesRead = recv(clientSocket, buffer, sizeof(buffer), 0);
+
+    if (bytesRead == -1) {
+        perror("Error while receiving data");
+    } else if (bytesRead == 0) {
+        cout << "Server disconnected." << endl;
+    } else {
+        // Null-terminate the received data to treat it as a string
+        buffer[bytesRead] = '\0';
+        cout << "> Receive RDMA device info from MASTER " << buffer << endl;
+    }
+
+
+    while(true) {
+        cout << "Waiting for UNLOCK from MASTER" << endl;
+        bytesRead = recv(clientSocket, buffer, sizeof(buffer), 0);
+
+        if (bytesRead == -1) {
+            perror("Error while receiving data");
+            break;
+        } else if (bytesRead == 0) {
+            cout << "Server disconnected." << endl;
+            break;
+        } else {
+            buffer[bytesRead] = '\0';
+            cout << "Received message from server: " << buffer << endl;
+            // Send data using RDMA
+            // check if i have data
+
+            cout << "Node unblocked to send data" << endl;
+            cout << "Checking if I have data to send to master node ..." << endl;
+
+            cout << "Send data through RDMA" << endl;
+        }
+    }
+
+
+    
+free_context:
+	ibv_close_device(context);
+
+free_devlist:
+	ibv_free_device_list(dev_list);
+
+close_socket:
+    close(clientSocket);
+	
     return 0;
 }
--- socket-server.cc
+++ socket-server.cc
@@ -7,6 +7,7 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <list>
+#include <fcntl.h> 
 using namespace std;
 
 const int PORT = 8080;
@@ -24,21 +25,53 @@ bool clientSocketExist(std::list<int> clients, int currentSocket) {
     return false;
 }
 
-// Function to handle a client connection
+int set_socket_non_blocking(int socket) {
+    int flags = fcntl(socket, F_GETFL, 0);
+    if (flags == -1) {
+        perror("fcntl F_GETFL failed");
+        return -1;
+    }
+
+    if (fcntl(socket, F_SETFL, flags | O_NONBLOCK) == -1) {
+        perror("fcntl F_SETFL failed");
+        return -1;
+    }
+
+    return 0;
+}
+
 void handleClient(int clientSocket) {
     char buffer[BUFFER_SIZE];
     
-    // Receive data from the client
     ssize_t bytesRead = recv(clientSocket, buffer, sizeof(buffer), 0);
 
     if (bytesRead == -1) {
         perror("Error while receiving data");
+        return;
     } else if (bytesRead == 0) {
         std::cout << "Client disconnected. Client socket: " << clientSocket << std::endl;
+        return;
     } else {
         // Null-terminate the received data to treat it as a string
+        cout << "> Receive RDMA device info from NODE." << endl;
+
         buffer[bytesRead] = '\0';
         std::cout << "Received message from client (Socket " << clientSocket << "): " << buffer << std::endl;
+        
+        cout << "< Send RDMA device info to NODE" << endl;
+        const char* responseMessage = "[SERVER] RDMA DEVICE INFO";
+        ssize_t bytesSent = send(clientSocket, responseMessage, strlen(responseMessage), 0);
+
+        if (bytesSent == -1) {
+            perror("Error while sending data");
+            return;
+        } 
+    }
+
+    if(!clientSocketExist(clients, clientSocket)) {
+        clients.push_back(clientSocket);
+        // Send client in non blocking because we will just send unlock message going further
+        set_socket_non_blocking(clientSocket);
     }
 }
 
@@ -88,13 +121,11 @@ void acceptConnections() {
             return;
         }
 
-        if(!clientSocketExist(clients, clientSocket)) {
-            cout << "Client connected to server with ip: " << ipString << " and port: " << clientAddr.sin_port << endl;
-            clients.push_back(clientSocket);
-        }
-
+        // Receive RDMA data when client first connects
         std::thread clientThread(handleClient, clientSocket);
         clientThread.detach();  // Detach the thread to allow it to run independently
+
+
     }
 
     // Close the server socket (unreachable in this example)
@@ -109,10 +140,39 @@ void cleanClientList() {
     clients.clear();
 }
 
+void rdma_communication() {
+    cout << "START RDMA COMMUNICATION" << endl;
+    while(true) {
+        for(const auto &client : clients) {
+            cout << "> Unlock client socket: " << client << " to send data" << endl;
+            const char* unlockMessage = "[SERVER] UNLOCK";
+            ssize_t bytesSent = send(client, unlockMessage, strlen(unlockMessage), 0);
+
+            if (bytesSent == -1) {
+                perror("Error while sending data");
+            } else {
+                std::cout << "Unlock successfully sent to client (Socket " << client << "): " << unlockMessage << std::endl;
+            }
+
+            // Pool data from RDMA for a small period of time
+            cout << "Pool for data from queue for client " << client << endl;
+
+            // Sleep before next client TODO Remove later
+            cout << "Sleep few seconds" << endl;
+            sleep(5);
+        }
+    }
+}
+
 int main() {
     std::thread serverThread(acceptConnections);
 
-    serverThread.join();
+    std::thread rdma_communication_thread(rdma_communication);
 
+    serverThread.join();
+    rdma_communication_thread.join();
+
+
+    cout << "End of main!" << endl;
     return 0;
 }
